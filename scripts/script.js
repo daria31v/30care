a26b06e2f184794d1ebc80ab98e4fb829de2c891
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2024-03-16 10:46:12 +0100

    new commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const dialog = document.getElementById("myDialog");
+const backdrop = document.getElementById("backdrop");
+const closeButton = document.getElementById("closeDialog");
+const openButtons = document.querySelectorAll(".openDialog");
+console.log(backdrop);
+
+openButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    dialog.showModal();
+    backdrop.style.display = "block";
+    openButtons.disabled = true;
+  });
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+  backdrop.style.display = "none";
+  openButtons.disabled = false;
+});
+
+
+
